532868c5740c157e2ba8e468501580e5f73d4bc9
Ranksai <ranks.j.inter1908@gmail.com>
2020-02-04 20:43:37 +0900

    add cycle, @ and end node

--- src/main.go
+++ src/main.go
@@ -13,7 +13,7 @@ import (
 const (
 	SampleData                    = "&x1@cycle(&x1 := {a:&x2}, &x2 := {b:&x2,c:&x3}, &x3 := &y)"
 	AtmarkSingle                  = "&x1@&x1"
-	AtmarkData                    = "&(&x1 := {a:&x2}, &x2 := {b:&x2,c:&x3}, &x3 := &y)@(&x1 := {a:&x2}, &x2 := {b:&x2,c:&x3}, &x3 := &y)"
+	AtmarkData                    = "(&x1 := {a:&x2}, &x2 := {b:&x2,c:&x3}, &x3 := &y)@(&x1 := {a:&x2}, &x2 := {b:&x2,c:&x3}, &x3 := &y)"
 	InclusionSampleData           = "(&x1 := {a:&x2}, &x2 := {b:&x2,c:&x3}, &x3 := &y)"
 	InclusionSampleDataNotInclude = "&x1 := {a:&x2}, &x2 := {b:&x2,c:&x3}, &x3 := &y"
 	InclusionSampleSingle         = "&x1 := {a:&x2}"
@@ -21,12 +21,25 @@ const (
 	InclusionSampleNone           = "&x3 := &y"
 )
 
+type Tree struct {
+	Name       string
+	StartNodes []string
+	EndNodes   []string
+	TreeNodes  Nodes
+}
+
 type Node struct {
 	Name     string
-	Id       int
-	Parent   []Node
-	Children map[string]string
+	Children []ChildNode
+	Eps      []string
 }
+
+type ChildNode struct {
+	Name  string
+	Label string
+}
+
+type ChildrenNodes = []ChildNode
 
 type Nodes = []Node
 
@@ -48,25 +61,26 @@ func calcInclusion(inclusionCurlyBracket string) (Node, error) {
 	// 複数移動するノードがあったときにカンマでわかる
 	splitResult = strings.Split(splitResult[1], ",")
 
-	moveMap := make(map[string]string, 0)
-
+	childrenNodes := make(ChildrenNodes, 0)
+	epsList := make([]string, 0)
 	// 移動するノード分確認
 	for i := range splitResult {
 		// 移動先を見る
 		if strings.Contains(splitResult[i], ":") {
-			moveNode := strings.Split(splitResult[i], ":")
-			moveMap[moveNode[0]] = moveNode[1]
+			childNode := strings.Split(splitResult[i], ":")
+			childrenNodes = append(childrenNodes, ChildNode{
+				Name:  childNode[1],
+				Label: childNode[0],
+			})
 		} else {
-			// TODO: ε 遷移が複数あるときに対応できない 今だと最後の ε 遷移のみ
 			// &x3 = &y のようなとき ε 遷移
-			moveMap["ε"] = splitResult[i]
+			epsList = append(epsList, splitResult[i])
 		}
 	}
 	node := Node{
 		Name:     nodeName,
-		Id:       0,
-		Parent:   nil,
-		Children: moveMap,
+		Children: childrenNodes,
+		Eps:      epsList,
 	}
 	return node, nil
 }
@@ -83,11 +97,83 @@ func multiCalcInclusion(inclusionCurlyBracket string) (Nodes, error) {
 	return nodes, nil
 }
 
-func main() {
-	result, err := multiCalcInclusion(InclusionSampleDataNotInclude)
+// cycleInput is not include "cycle"
+func calcCycle(cycleInput string) (Nodes, error) {
+	nodes, err := multiCalcInclusion(cycleInput)
 	if err != nil {
-		fmt.Printf("Err func main: %+w", err)
+		return nil, err
 	}
-	fmt.Println("print node")
-	fmt.Println(result)
+	for i := range nodes {
+		nodes[i].Eps = append(nodes[i].Eps, nodes[i].Name)
+	}
+	return nodes, nil
+}
+
+func calcAtmark(left, right string, tree Tree) (Tree, error) {
+	if strings.Contains(left, ",") {
+	} else {
+		tree.StartNodes = append(tree.StartNodes, left)
+	}
+	return tree, nil
+}
+
+func checkEndNodes(tree Tree) Tree {
+	childNodeName := make([]string, 0)
+	endNodes := make([]string, 0)
+	for _, node := range tree.TreeNodes {
+		for _, childNode := range node.Children {
+			childNodeName = append(childNodeName, childNode.Name)
+		}
+		for _, eps := range node.Eps {
+			childNodeName = append(childNodeName, eps)
+		}
+	}
+	for i := range childNodeName {
+		flag := true
+		for _, node := range tree.TreeNodes {
+			if childNodeName[i] == node.Name {
+				flag = false
+				break
+			}
+		}
+		if flag {
+			endNodes = append(endNodes, childNodeName[i])
+		}
+	}
+	tree.EndNodes = endNodes
+	return tree
+}
+
+func calc(input string, tree Tree) (Tree, error) {
+	if strings.Contains(input, "@") {
+		splits := strings.Split(input, "@")
+		// cycle 側の計算
+		cycleInput := strings.Trim(splits[1], "cycle")
+		cycleInput = strings.TrimLeft(cycleInput, "(")
+		cycleInput = strings.TrimRight(cycleInput, ")")
+		nodes, err := calcCycle(cycleInput)
+		if err != nil {
+			return tree, err
+		}
+		// @ の計算
+		tree, _ = calcAtmark(splits[0], "", tree)
+		tree.TreeNodes = nodes
+		// endNode の確認
+		tree = checkEndNodes(tree)
+	}
+	return tree, nil
+}
+
+func main() {
+	// init tree
+	tree := Tree{
+		Name:       "UnCAL Tree",
+		StartNodes: nil,
+		EndNodes:   nil,
+		TreeNodes:  nil,
+	}
+	// input tree data
+	input := SampleData
+	tree, _ = calc(input, tree)
+	fmt.Printf("%+v", tree)
 }
